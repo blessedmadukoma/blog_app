e0a4eb185606e1b278d852d5d610c68fb3117296
Madukoma Blessed <blessedmadukoma@gmail.com>
2021-02-17 07:21:14 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -16,19 +16,19 @@ class BlogDetailView(DetailView):
   model = Post
   template_name = 'post_detail.html'
 
-
+@login_required
 class BlogCreateView(CreateView):
   model = Post
   template_name = 'post_new.html'
   fields = ['title', 'author', 'body']
 
-
+@login_required
 class BlogUpdateView(UpdateView):
   model = Post
   template_name = 'post_edit.html'
   fields = ['title', 'body']
 
-
+@login_required
 class BlogDeleteView(DeleteView):
   model = Post
   template_name = 'post_delete.html'
